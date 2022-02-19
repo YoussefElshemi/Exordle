64590e4ac47ec0aae35566977bad16581b7ed6a2
theosantos3 <theosantos3@gmail.com>
2022-02-19 21:26:31 +0000

    some css work: grid for guessing and background colour

--- src/game/static/js/script.js
+++ src/game/static/js/script.js
@@ -6,9 +6,49 @@ for (const form of document.forms) {
       const next = target.nextElementSibling;
       const nextParent = target.parentElement.nextElementSibling;
 
-      if (next && next.nodeName === "INPUT") return next.focus();
-      if (!next && nextParent) return nextParent.children[1].focus();
+      if (next && next.nodeName === "INPUT") next.focus();
+
+      if (!next) {
+        const formData = new FormData(form);
+        const data = {
+          ...Object.fromEntries(formData.entries()),
+          csrfmiddlewaretoken: getCookie("csrftoken")
+        };
+        
+        $.ajax({
+          type: "POST",
+          url: "/check",
+          data,
+          success: responseJSON => {
+            responseJSON = JSON.parse(responseJSON);
+          
+            for (const [position, value] of Object.entries(responseJSON)) {
+              const idx = Number(position);
+              const inputElement = form.children[idx];
+              inputElement.classList.add(value);
+            }
+          },
+          error: data => {
+            console.log(data);
+          }
+        });
+
+        nextParent.children[1].focus();
+      }
     }
 
   });
+}
+
+function getCookie(name) {
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = jQuery.trim(cookies[i]);
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        return decodeURIComponent(cookie.substring(name.length + 1));
+      }
+    }
+  }
 }
--- src/game/urls.py
+++ src/game/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('play', views.game, name='game')
+    path('play', views.game, name='game'),
+    path('check', views.check, name='check'),
+
 ]
