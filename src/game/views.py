efe26eb951eacd4cd2b514f95e85fe629b007593
Youssef Elshemi <ye216@exeter.ac.uk>
2022-02-19 22:11:51 +0000

    pylint stuff

--- src/game/views.py
+++ src/game/views.py
@@ -4,29 +4,29 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'game/index.html', {})
-  
+
 def game(request):
     word = 'FORUM'
     length = len(word) + 1
-    
+
     context = {
       'word_range': range(1, length),
       'range': range(1, length + 1)
     }
-    
+
     return render(request, 'game/play.html', context)
-  
+
 def check(request):
     word = 'FORUM'
     request.POST._mutable = True
-    
+
     request.POST.pop("guess")
     request.POST.pop("csrfmiddlewaretoken")
-    
+
     request.POST._mutable = False
 
     data = {}
-    for position, value in request.POST.items():        
+    for position, value in request.POST.items():
         idx = int(position) - 1
         if word[idx] == value.upper():
             data[position] = "perfect"
@@ -36,6 +36,6 @@ def check(request):
             word = word.replace(value.upper(), " ")
         else:
             data[position] = "wrong"
-                
+
     return HttpResponse(json.dumps(data))
  
